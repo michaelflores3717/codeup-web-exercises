fda0ce47b3766ee53343676bd06f0876f7215a99
Mike Flores <mike.flores3717@gmail.com>
2019-02-14 16:33:50 -0600

    Adding testing files

---
+++ intro-to-testing-js/tests.js
@@ -0,0 +1,29 @@
+// Unit tests for the helloWorld function
+describe('helloWorld', function() {
+    it('should be a defined function', function() {
+      expect(typeof helloWorld).toBe('function');
+    });
+    it('should return a string when called', function() {
+      expect(typeof helloWorld()).toBe("string");
+    });
+    it('should return the string "Hello, World!" when executed', function() {
+      expect(helloWorld()).toBe("Hello, World!");
+    });
+    it("should never return 'undefined' when called", function() {
+      expect(helloWorld()).not.toBe(undefined);
+    });
+});
+
+
+describe('sayHello', function() {
+    it('should be a defined function', function() {
+        expect(typeof sayHello).toBe('function');
+    });
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it("should say 'Hello Jane' when passed input 'Jane'", function() {
+        expect(sayHello()).toBe("Hello Jane!");
+    });
+});
+
