f7db2af10a0855ddd586cb04933a1bebde5e0840
Mike Flores <mike.flores3717@gmail.com>
2019-02-14 15:19:38 -0600

    Adding codeup project

--- js/miniproblems.js
+++ js/miniproblems.js
@@ -1,15 +1,6 @@
-function isShortWord (word) {
-    return word.length < 5;
+function sum(numbers) {
+    return numbers.reduce(function(a,b) {
+        return a + b
+    });
 }
 
-function isSameLength (stringOne, stringTwo) {
-    return stringOne.length === stringTwo.length;
-}
-
-function getSmallerSegment (string, number) {
-    return string.substring(0, number).toLowerCase();
-}
-
-console.log(isShortWord("cat"));
-console.log(isSameLength("bob", "dog"));
-console.log(getSmallerSegment("animal", 2));
