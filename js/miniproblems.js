29031eb105abe6ec6dd132df588ea11a5e5457b1
Mike Flores <mike.flores3717@gmail.com>
2019-02-15 15:14:39 -0600

    Adding changes

--- js/miniproblems.js
+++ js/miniproblems.js
@@ -27,3 +27,37 @@ users.forEach(function(element, index, array) {
     element.age = element.age + 1;
     console.log(element.age);
 });
+
+var dog =
+    {
+        breed: "Corgi",
+        weightInPounds: 10,
+        age: 5,
+        color: "brown",
+        sterilized: false,
+        shotRecords: [{
+            date: 2009,
+            typeOfShot: "rabies"
+        },
+        {
+            date: 2016,
+            typeOfShot: "rabies"
+        }],
+        bark: function(){
+            console.log("Woof!")
+        },
+        getOlder: function(){
+             this.age += 1;
+        },
+        fixed: function() {
+            this.sterilized = true;
+        },
+        vaccinate: function(typeofShot){
+            var date = new Date();
+            var shotObject = {
+                date: date,
+                typeOfShot: typeofShot
+            };
+            this.shotRecords.push(shotObject);
+        }
+    };
