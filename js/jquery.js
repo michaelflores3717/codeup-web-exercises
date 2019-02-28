d1d35ace5f94f0586d9a1a3bfa088f374aa32739
Mike Flores <mike.flores3717@gmail.com>
2019-02-28 14:18:52 -0600

    committing

--- js/jquery.js
+++ js/jquery.js
@@ -2,6 +2,37 @@
 
 $(document).ready(function() {
 
-    alert("DOM has finished loading!");
+    var contents = $('#header').html();
+    alert(contents);
+
+    var contentsTwo = $('#p').html();
+    alert(contentsTwo);
+
+    $('.codeup').css('border', '1px solid red');
+
+    $('.codeup').css('background-color', '#FFFF00')
+
+    $('li').css('font-size', '20px');
+
+    var contents = $('h1').html();
+    alert(contents);
+
+    $('h1').click(function() {
+        $('h1').css('background-color', "red");
+    });
+
+    $('p').dblclick(function() {
+        $(this).css('font-size', "18px");
+    });
+
+    function mouseOn() {
+        $(this).css('color', "red");
+    }
+
+    function mouseOff() {
+        $(this).css('color', "black");
+    }
+
+    $('li').hover(mouseOn, mouseOff);
 
 });
